fb7e6ba711d6a9ce30bfb8445e66891fe99217db
analistaRoberto <analistaroberto.pena@gmail.com>
2023-08-21 23:42:24 -0400

    correcion primer entregable

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola mundo</Text>
+      <Text>Primeras Pruebas</Text>
+      <Text>Curso de Desarrollo de Aplicacion</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <Text style={styles.title}>
+        The title and onPress handler are required. It is recommended to set
+        accessibilityLabel to help make your app usable by everyone.
+      </Text>
+     
     </View>
+
+    
   );
 }
 
@@ -16,5 +29,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    color: 'red',
+  },
+ Button: {
+  width: 50,
+  height: 50,
+  color: 'yellow',
+  backgroundColor: 'brown',
+ },
+
 });
